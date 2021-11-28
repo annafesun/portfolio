b835442bbda8b3e8df78ade4e25988316f5b6272
annafesun <aniutafes@gmail.com>
2021-11-28 19:26:23 +0200

    Updates

---
+++ precache-manifest.d6b2518f89e83176582cf57d764c5f06.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70ebd4eed17f49eb0e6f785745b630a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4f215da0a07c9a5caa91",
+    "url": "/portfolio/static/css/main.9cd1a027.chunk.css"
+  },
+  {
+    "revision": "d4c296abced1acd05628",
+    "url": "/portfolio/static/js/2.861109ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.861109ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f215da0a07c9a5caa91",
+    "url": "/portfolio/static/js/main.eb7bcb7d.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  },
+  {
+    "revision": "489468903bb3783a806d513dd873a5c4",
+    "url": "/portfolio/static/media/Goldfather.48946890.woff2"
+  },
+  {
+    "revision": "7290f02dcdd9af2e9aca4193194158f4",
+    "url": "/portfolio/static/media/Goldfather.7290f02d.woff"
+  },
+  {
+    "revision": "29a377c22e030e54543ea7fe0bac34bb",
+    "url": "/portfolio/static/media/SundayBest.29a377c2.woff"
+  },
+  {
+    "revision": "34700c126c2c8cc27ce0976637660e1d",
+    "url": "/portfolio/static/media/SundayBest.34700c12.woff2"
+  },
+  {
+    "revision": "23652fc22bd8e83985a3c4b7c8858cc5",
+    "url": "/portfolio/static/media/avatar.23652fc2.jpeg"
+  },
+  {
+    "revision": "122641c3a9f85cf8202c947aea4fddd3",
+    "url": "/portfolio/static/media/counter.122641c3.png"
+  },
+  {
+    "revision": "b29324c45ced02ca418e097ae8f52c77",
+    "url": "/portfolio/static/media/ico-antd.b29324c4.svg"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/portfolio/static/media/ico-bootstrap.73844123.png"
+  },
+  {
+    "revision": "d0eceb1b571703f4d732ba26f569b1a3",
+    "url": "/portfolio/static/media/ico-css.d0eceb1b.png"
+  },
+  {
+    "revision": "aac4650a77ca30f2fda52d0ab1d8b609",
+    "url": "/portfolio/static/media/ico-js.aac4650a.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/portfolio/static/media/ico-react.47ce6e77.png"
+  },
+  {
+    "revision": "44993df475fac4e52de04fa7d239480a",
+    "url": "/portfolio/static/media/polind.44993df4.png"
+  }
+]);
